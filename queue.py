0966270c2140f0598393be7e5b899113e833ec84
setsura3 <setsura3@gmail.com>
2019-08-31 19:23:56 -0700

    Update queue.py

--- queue.py
+++ queue.py
@@ -1,1 +1,22 @@
 
+class Queue():
+    def __init__(self):
+        self.ele = []
+
+    def enqueue(self, ele):
+        self.ele.append(ele)
+
+    def dequeue(self):
+        return self.ele.pop(0)
+
+    def getQueue(self):
+        return self.ele
+
+    def getSize(self):
+        return len(self.ele)
+
+    def isEmpty(self):
+        return self.ele == []
+
+    def getFirst(self):
+        return self.ele[0]
